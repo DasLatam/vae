bb9c4a15af17665ba7665aab73100e7370567d23
DAS Latam <daslatamarg@gmail.com>
2025-09-16 23:12:01 +0000

    data: update political parties list with official 2025 data

--- src/lib/datosElectorales.ts
+++ src/lib/datosElectorales.ts
@@ -1,30 +1,222 @@
 // src/lib/datosElectorales.ts
 
 export const partidosPorProvincia: Record<string, string[]> = {
-  "Buenos Aires": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Catamarca": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Chaco": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Chubut": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Ciudad Autónoma de Buenos Aires": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Córdoba": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Corrientes": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Entre Ríos": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Formosa": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Jujuy": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "La Pampa": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "La Rioja": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Mendoza": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Misiones": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Neuquén": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Río Negro": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Salta": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "San Juan": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "San Luis": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Santa Cruz": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Santa Fe": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria", "Frente de Izquierda"],
-  "Santiago del Estero": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Tierra del Fuego": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
-  "Tucumán": ["La Libertad Avanza", "Juntos por el Cambio", "Unión por la Patria"],
+  "Buenos Aires": [
+    "UNION FEDERAL",
+    "NUEVOS AIRES",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "POTENCIA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FUERZA PATRIA",
+    "PROVINCIAS UNIDAS",
+    "COALICION CIVICA - AFIRMACION PARA UNA REPUBLICA IGUALITARIA (ARI)",
+    "FRENTE PATRIOTA FEDERAL",
+    "UNION LIBERAL",
+    "PARTIDO NUEVO BUENOS AIRES",
+    "MOVIMIENTO AVANZADA SOCIALISTA",
+    "MOVIMIENTO POLITICO, SOCIAL Y CULTURAL PROYECTO SUR",
+    "LIBER.AR"
+  ],
+  "Ciudad Autónoma de Buenos Aires": [
+    "ALIANZA LA LIBERTAD AVANZA",
+    "ALIANZA POTENCIA",
+    "FUERZA PATRIA",
+    "CIUDADANOS UNIDOS",
+    "HAGAMOS FUTURO",
+    "FRENTE DE IZQUIERDA Y DE TRABAJadores - UNIDAD",
+    "PARTIDO FEDERAL",
+    "GEN",
+    "INSTRUMENTO ELECTORAL POR LA UNIDAD POPULAR",
+    "FRENTE PATRIOTA FEDERAL",
+    "PARTIDO SOCIALISTA AUTENTICO",
+    "MOVIMIENTO DE JUBILADOS Y JUVENTUD",
+    "INTEGRAR",
+    "LA IZQUIERDA EN LA CIUDAD",
+    "MOVIMIENTO PLURAL"
+  ],
+  "Catamarca": [
+    "FUERZA PATRIA",
+    "SOMOS PROVINCIAS UNIDAS - CATAMARCA",
+    "HACEMOS RENACER CATAMARCA",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "PRIMERO CATAMARCA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD"
+  ],
+  "Chaco": [
+    "FRENTE FUERZA PATRIA",
+    "VAMOS CHACO",
+    "ALIANZA LA LIBERTAD AVANZA"
+  ],
+  "Chubut": [
+    "LA FUERZA DEL TRABAJO CHUBUTENSE",
+    "DESPIERTA CHUBUT",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FRENTE UNIDOS PODEMOS",
+    "GEN",
+    "LA LIBERTAD AVANZA",
+    "PARTIDO LIBERTARIO"
+  ],
+  "Córdoba": [
+    "DEFENDAMOS CORDOBA",
+    "CIUDADANOS",
+    "CORDOBA TE QUIERO",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "ENCUENTRO POR LA REPUBLICA",
+    "PROVINCIAS UNIDAS",
+    "FUERZA PATRIA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "MOVIMIENTO AVANZADA SOCIALISTA"
+  ],
+  "Corrientes": [
+    "LA LIBERTAD AVANZA",
+    "VAMOS CORRIENTES",
+    "CORRIENTES NOS UNE",
+    "FUERZA PATRIA"
+  ],
+  "Entre Ríos": [
+    "FUERZA ENTRE RÍOS",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "AHORA 503",
+    "MOVIMIENTO AL SOCIALISMO",
+    "PARTIDO SOCIALISTA",
+    "NUEVA IZQUIERDA"
+  ],
+  "Formosa": [
+    "FRENTE DE LA VICTORIA",
+    "JUNTOS POR LA LIBERTAD Y LA REPUBLICA",
+    "ALIANZA LA LIBERTAD AVANZA"
+  ],
+  "Jujuy": [
+    "FRENTE PRIMERO JUJUY AVANZA",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FRENTE JUJUY CRECE",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "TRANSFORMACION LIBERTARIA",
+    "FRENTE FUERZA PATRIA",
+    "FRENTE LIBERAL"
+  ],
+  "La Pampa": [
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FRENTE DEFENDEMOS LA PAMPA",
+    "CAMBIA LA PAMPA",
+    "MOVIMIENTO AL SOCIALISMO"
+  ],
+  "La Rioja": [
+    "FEDERALES DEFENDAMOS LA RIOJA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "LAS FUERZAS DEL CENTRO",
+    "PROVINCIAS UNIDAS"
+  ],
+  "Mendoza": [
+    "FUERZA JUSTICIALISTA MENDOZA",
+    "FRENTE VERDE",
+    "ALIANZA DEFENDAMOS MENDOZA - PROVINCIAS UNIDAS",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FRENTE LIBERTARIO DEMOCRATA",
+    "MOVIMIENTO AL SOCIALISMO",
+    "PROTECTORA FUERZA POLITICA"
+  ],
+  "Misiones": [
+    "RENOVADOR DE LA CONCORDIA NEO",
+    "FRENTE POR LA VIDA Y LOS VALORES, POR UN NUEVO OCTUBRE",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FUERZA PATRIA",
+    "FRENTE POPULAR AGRARIO Y SOCIAL",
+    "UNION CIVICA RADICAL",
+    "PARTIDO DEL OBRERO"
+  ],
+  "Neuquén": [
+    "LA NEUQUINIDAD",
+    "FUERZA PATRIA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "MOVIMIENTO AL SOCIALISMO",
+    "INSTRUMENTO ELECTORAL POR LA UNIDAD POPULAR",
+    "LA LIBERTAD AVANZA",
+    "FUERZA LIBERTARIA",
+    "MAS POR NEUQUEN"
+  ],
+  "Río Negro": [
+    "JUNTOS DEFENDEMOS RIO NEGRO",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FUERZA PATRIA",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "MOVIMIENTO AL SOCIALISMO",
+    "PRO-PROPUESTA REPUBLICANA",
+    "PRIMERO RIO NEGRO"
+  ],
+  "Salta": [
+    "PRIMERO LOS SALTEÑOS",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FUERZA PATRIA",
+    "MOVIMIENTO AL SOCIALISMO",
+    "PARTIDO DE LA VICTORIA",
+    "LA LIBERTAD AVANZA"
+  ],
+  "San Juan": [
+    "FUERZA SAN JUAN",
+    "POR SAN JUAN",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "CRUZADA RENOVADORA"
+  ],
+  "San Luis": [
+    "FRENTE PUEBLO",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "PROVINCIAS UNIDAS",
+    "FRENTE JUSTICIALISTA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "MOVIMIENTO AL SOCIALISMO"
+  ],
+  "Santa Cruz": [
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "FUERZA SANTACRUCEÑA",
+    "POR SANTA CRUZ",
+    "MOVIMIENTO AL SOCIALISMO",
+    "COALICION CIVICA - AFIRMACION PARA UNA REPUBLICA IGUALITARIA (ARI)",
+    "PRO-PROPUESTA REPUBLICANA",
+    "LA LIBERTAD AVANZA",
+    "PROYECTO ALTERNATIVO"
+  ],
+  "Santa Fe": [
+    "FRENTE AMPLIO POR LA SOBERANIA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "PROVINCIAS UNIDAS",
+    "DEFENDAMOS SANTA FE",
+    "FUERZA PATRIA",
+    "MOVIMIENTO AL SOCIALISMO",
+    "COMPROMISO FEDERAL",
+    "MOVIMIENTO INDEPENDIENTE RENOVADOR"
+  ],
+  "Santiago del Estero": [
+    "FRENTE CÍVICO POR SANTIAGO",
+    "FRENTE FUERZA PATRIA PERONISTA",
+    "DESPIERTA SANTIAGO",
+    "HACEMOS POR SANTIAGO",
+    "NUEVA IZQUIERDA",
+    "LA LIBERTAD AVANZA",
+    "UNITE POR LA LIBERTAD Y DIGNIDAD"
+  ],
+  "Tierra del Fuego": [
+    "DEFENDAMOS TIERRA DEL FUEGO",
+    "PROVINCIAS UNIDAS",
+    "FUERZA PATRIA",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "MOVIMIENTO AL SOCIALISMO",
+    "PARTIDO FRENTE GRANDE",
+    "FRENTE PATRIOTA FEDERAL"
+  ],
+  "Tucumán": [
+    "FRENTE TUCUMAN PRIMERO",
+    "ALIANZA LA LIBERTAD AVANZA",
+    "FRENTE DE IZQUIERDA Y DE TRABAJADORES - UNIDAD",
+    "UNIDOS POR TUCUMAN",
+    "PARTIDO DEL TRABAJO Y DEL PUEBLO",
+    "FRENTE DEL PUEBLO UNIDO",
+    "FUERZA REPUBLICANA",
+    "POLITICA PARA LA CLASE OBRERA"
+  ]
 };
 
 export const provinciasArgentinas: string[] = [
