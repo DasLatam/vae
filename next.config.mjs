4e241ba0c66c6aff6d3c9ec5381854732995ee42
DAS Latam <daslatamarg@gmail.com>
2025-09-16 14:38:09 +0000

    fix: clean and restore final project configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 // next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Esta configuración deshabilita el chequeo de ESLint durante el 'build'.
-  // Permite que el deploy en Vercel se complete sin ser bloqueado por errores de linting.
   eslint: {
     ignoreDuringBuilds: true,
   },
